93bdf33d7a1728a13cbbd2539ae3abbcada2e123
rrtrenchf <65308337+rrtrenchf@users.noreply.github.com>
2020-07-01 22:07:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,160 +1,152 @@
+
+var input = ''
+var passArray = {
+  alphaUpperisT :"ABCDEFGHIJKLMNOPRSTUVWXYZ",
+  alphaLowerisT : "abcdefghijklmnopqrstuvwxyz",
+  numisT :"1234567890",
+  specisT : "!@#$%^&*()"
+  
+}
+ 
+console.log(passArray.alphaUpperisT[3])
+                //get password length
+//   input = prompt("What is your Password length? Must be between 8 and 128 characters");
+// while (input< 8 || input> 128 || isNaN(input))
+// (input =prompt("What is your Password length? Must be between 8 and 128 characters"));
+
+
+
+// if (input > 8 || input < 128); alert( "You have chosen " +input+ " characters");
+// console.log(input);
+
+
+
+
+
+
+// {
+
+//                //get complexity values
+
+// if (confirm("Do you want upper case letters?")) {
+// console.log(true)
+// userChoices= passArray.alphaUpperisT +userChoices
+// }
+
+
+// if (confirm( "Add lower Case Letters?") ===true) {
+//     console.log(true);
+//    userChoices= passArray.alphaLowerisT + userChoices
+// } 
+    
+// if (confirm("Add numbers?") === true) {
+//     console.log(true);
+//     userChoices= passArray.numisT + userChoices
+// }
+    
+// if (confirm("Add Special Characters?") === true) {
+//     console.log(true);
+
+//     userChoices = passArray.specisT + userChoices
+    
+
+// }
+// if (userChoices=== '') {
+//   alert("Much choose at least one")
+// }
+// console.log(userChoices)
+
+
+// }
+
+
 // Assignment Code
-//hoook into button
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword)
 
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+    var passArray = {
+        alphaUpperisT :"ABCDEFGHIJKLMNOPRSTUVWXYZ",
+        alphaLowerisT : "abcdefghijklmnopqrstuvwxyz",
+        numisT :"1234567890",
+        specisT : "!@#$%^&*()"
+        
+    }
+    var userChoices= ''
+                    //get password length
+  input = prompt("What is your Password length? Must be between 8 and 128 characters");
+  while (input< 8 || input> 128 || isNaN(input))
+  (input =prompt("What is your Password length? Must be between 8 and 128 characters"));
+  
+  
+  
+  if (input > 8 || input < 128); alert( "You have chosen " +input+ " characters");
+  console.log(input);
+  
+  
+  
+  
+  
+  
+  {
+  
+                 //get complexity values
+  
+  if (confirm("Do you want upper case letters?")) {
+  console.log(true)
+  userChoices= passArray.alphaUpperisT +userChoices
+  }
+  
+  
+  if (confirm( "Add lower Case Letters?") ===true) {
+      console.log(true);
+     userChoices= passArray.alphaLowerisT + userChoices
+  } 
+      
+  if (confirm("Add numbers?") === true) {
+      console.log(true);
+      userChoices= passArray.numisT + userChoices
+  }
+      
+  if (confirm("Add Special Characters?") === true) {
+      console.log(true);
+  
+      userChoices = passArray.specisT + userChoices
+      
+  
+  }
+  if (userChoices=== '') {
+    alert("Much choose at least one")
+    return;
+  }
+  console.log(userChoices)
+  
+  
+  }
+  
+
+    console.log(passArray.alphaUpperisT[3])
+
+    var password = ""
+
+  for (var i = 0; i<input;i++) {
+      password =userChoices.charAt(Math.floor(Math.random()*userChoices.length))+password;
+      
+  
+ 
+  }
+  console.log(password)
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
 
-
-// When button is clicked call the right password function
-generateBtn.addEventListener("click", writePassword);
   
 
 
 
-var  createPassword = {
-  length: " ",
-  alphaUpper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  alphaLower: "abcdefghijklmnopqrstuvwxyz",
-  num: 1234567890,
-  special: "!@#$%^&*()_+",
   }
 
-
-    
-//   // } else {
-//   //   prompt ("Must be between 8 and 128 characters")
-    
-// }
-  // obtain password length value. Store in console
-  while (createPassword[length] !== ''){
-    
-  // Prompt user to input a value
-  
-    createPassword[length] = prompt("Type password character length must be a number between 8 and 128 characters");
-    
-  if( createPassword[length] > 7 && createPassword[length] < 129){
-    function getLength() {
-      createPassword.push(createPassword[length]);
-      console.log(getLength)
-    }
-    
-  // Allowed value accepted and logged in console. with break if appropriate
-    
-    alert("you have chosen " +createPassword[length]+ " characters");
-    console.log(createPassword[length])
-  // push password length to array
-  
-  break;
-  
-
-    
-  
-  // password.push(password[length])
-    
-    
-  
-  // inappropriate value. alerted to try again
-   
-    }else if 
-    
-      (isNaN) alert ("must be a number between 8 and 128 characters");{
-    }
-    
-  }
-  
-  // asks if upper case letters are included. Logs response
-    
-    if (confirm([alphaUpper] = "Add Upper Case Letters?") == true) {
-    console.log(true);
-    
-    }else {
-    console.log(false);
-     // Asks if lower case letters are included. Logs response
-    }
-    if(confirm([alphaLower] = "Add Lower Case Letters?") == true) {
-    console.log(true);
-    }else {
-    console.log(false);
-    //Asks if numbers are included. Logs response.
-    }
-    if (confirm([num] = "Add Numbers?") == true) {
-   console.log(true);
-    }else {
-   console.log(false);
-    }
-    //ask if special characters are included. Logs response
-
-    if (confirm([special] = "Add Special Characters?") == true) {
-    console.log(true);
-
-    }else {
-    console.log(false);{
-    
-    
-
-    }
-   if (confirm([alphaUpper && alphaLower && num && special]) !==true){
-    alert("Must choose at least one character type!")
-    
-    }else{ 
-      
-      }
-      
-      
-}
-
-  
-  
-    
-      
-    
-
-
-  
-        
-    
-        
-      
-    
-    // break;
-     
-      
-   
-   
-    
-  
-
-      
-
-
-  
-
-  
-  
-  
-
-
-
-
-    
-// } if {
-//   password[alphaUpper] = confirm("Do You Want uppercase letters?");
-// }   if {
-//   password[alphaLower] = confirm("Do You Want lowercase letters?");
-// }   if {
-//   password[num] = confirm("Do You Want numbers?");
-// }   if {
-//   password[special] = confirm("Do You Want special characters?");
-// }   if {
-      
-// }
-    
